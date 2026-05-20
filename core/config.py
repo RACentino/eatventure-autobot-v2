6afec424402c54c6e64fa3afa35c08345d19f6a5
RACentino <rayarvincentino@gmail.com>
2026-05-21 07:20:14 +0800

    Config changes

--- core/config.py
+++ core/config.py
@@ -337,10 +337,10 @@ MAX_SCROLL_CYCLES = 5
 SCROLL_INCREMENT_STEP = 2
 
 # Delay between scroll intervals.
-SCROLL_INTERVAL_PAUSE = 0.250
+SCROLL_INTERVAL_PAUSE = 0.150
 
 # Settling delay after a scroll gesture.
-POST_SCROLL_SETTLE = 0.250
+POST_SCROLL_SETTLE = 0.150
 
 # Duration of each scroll drag gesture.
 SCROLL_DURATION = 0.250
