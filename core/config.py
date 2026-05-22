a22cbbdff5786d3ec89635892afaf63b9091dcc1
RACentino <rayarvincentino@gmail.com>
2026-05-22 10:17:20 +0800

    Config changes

--- core/config.py
+++ core/config.py
@@ -139,19 +139,19 @@ RED_ICON_FAST_MIN_DISTANCE = 30
 CLICK_DELAY = 0.075
 
 # Delay after cursor movement.
-MOUSE_MOVE_DELAY = 0.120
+MOUSE_MOVE_DELAY = 0.075
 
 # Duration to hold the mouse button down during click actions.
-MOUSE_DOWN_DURATION = 0.100
+MOUSE_DOWN_DURATION = 0.055
 
 # Delay after releasing the mouse button.
-MOUSE_UP_DURATION = 0.100
+MOUSE_UP_DURATION = 0.055
 
 # Enables hover movement before click actions.
 HOVER_ENABLED = True
 
 # Duration for hover movement before click actions.
-HOVER_DURATION = 0.150
+HOVER_DURATION = 0.075
 
 # Delay between upgrade station search attempts.
 UPGRADE_SEARCH_INTERVAL = 0.034
