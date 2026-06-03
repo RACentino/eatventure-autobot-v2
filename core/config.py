1c8e620701d371a491bec6836538960a537b35d0
RACentino <rayarvincentino@gmail.com>
2026-06-03 18:43:26 +0800

    CC

--- core/config.py
+++ core/config.py
@@ -160,10 +160,10 @@ HOVER_ENABLED = True
 HOVER_DURATION = 0.1
 
 # Delay between upgrade station search attempts.
-UPGRADE_SEARCH_INTERVAL = 0.1
+UPGRADE_SEARCH_INTERVAL = 0.3
 
 # Delay between state-machine actions.
-STATE_DELAY = 0.1
+STATE_DELAY = 0.3
 
 # Settling delay before verifying an upgrade station hold target.
 UPGRADE_STATION_VERIFY_SETTLE_DELAY = 0.150
