e9b6961949b9399fd00a6213c32f1ea64e534c50
RACentino <rayarvincentino@gmail.com>
2026-06-06 03:39:51 +0800

    CC

--- config.py
+++ config.py
@@ -275,13 +275,13 @@ UPGRADE_RED_ICON_Y_MAX = 680
 # Scrolling
 
 # Pixel distance for each scroll drag.
-SCROLL_PIXEL_STEP = 180
+SCROLL_PIXEL_STEP = 100
 
 # Multiplier applied to scroll pixel distance.
 SCROLL_DISTANCE_RATIO = 1.0
 
 # Maximum oscillating scroll cycles before reset.
-MAX_SCROLL_CYCLES = 2
+MAX_SCROLL_CYCLES = 4
 
 # Step increment applied to each oscillating scroll cycle.
 SCROLL_INCREMENT_STEP = 2
