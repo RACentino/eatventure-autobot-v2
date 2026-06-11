0b6de496e73f6246a62a3c881e3310cae09509f1
RACentino <rayarvincentino@gmail.com>
2026-06-12 04:57:17 +0800

    CC

--- config.py
+++ config.py
@@ -177,7 +177,7 @@ BOX_HSV_RANGES = (
 )
 
 # Minimum HSV mask ratio for accepting a box candidate.
-BOX_HSV_MIN_MATCH_RATIO = 0.4
+BOX_HSV_MIN_MATCH_RATIO = 0.5
 
 # Template confidence threshold for unlock button scans.
 UNLOCK_THRESHOLD = 0.905
