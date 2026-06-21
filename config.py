67ad61c1068641a4c87a310a8aa588e0aaa2ea8e
RACentino <rayarvincentino@gmail.com>
2026-06-21 12:29:40 +0800

    Config Changes

--- config.py
+++ config.py
@@ -173,6 +173,8 @@ RED_ICON_FAST_MODE_ENABLED = False
 # Red icon template names used when fast mode is enabled.
 RED_ICON_FAST_TEMPLATE_NAMES = (
         "RedIcon6",
+        "RedIcon12",
+        "RedIcon15",
 )
 
 # Minimum pixel distance between fast-mode red icon matches.
