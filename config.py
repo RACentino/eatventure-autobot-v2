b1bcfe9319ef09e4c4eea50461e69741c6c7dcbc
RACentino <rayarvincentino@gmail.com>
2026-08-11 22:27:03 +0800

    Adjust Scroll Settings

--- config.py
+++ config.py
@@ -246,7 +246,7 @@ UPGRADE_RED_ICON_Y_MAX = 680
 # Scrolling
 
 # Pixel distance for each scroll drag.
-SCROLL_PIXEL_STEP = 180
+SCROLL_PIXEL_STEP = 130
 
 # Multiplier applied to scroll pixel distance.
 SCROLL_DISTANCE_RATIO = 1.0
